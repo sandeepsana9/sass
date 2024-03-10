19b851d4c12cb9f0fa794c3527935bd704016f53
sandeep_sana <sandeep.sana@agamitechnologies.in>
2024-03-10 15:41:52 +0530

    sass

---
+++ sass/src/App.jsx
@@ -0,0 +1,29 @@
+import '../style/style.scss';
+
+function App() {
+
+  return (
+    <>
+    <div className='productCard'>
+      <a href="">anchor tag</a>
+      <h2>Product Title: Counter App </h2>
+    </div>
+    <div className='colors'>
+      <div className='color'>
+        all colors here
+      </div>
+    </div>
+    <div className='use-colors'>
+      <div className='use-color'>
+        all colors here
+      </div>
+    </div>
+    <button>click</button>
+    <p className='p1'>click p</p>
+    <p className='p2'>click p</p>
+    <p className='p3'>click p</p>
+    </>
+  )
+}
+
+export default App
